7fde88825c3eb7bea33c340845d6ea98200dd4dd
Partha Majumder <parthamajumder9810@gmail.com>
2025-04-24 00:58:52 +0530

    new js programming

--- handlerFile.js
+++ handlerFile.js
@@ -1,9 +1,27 @@
-const fs = require('fs'); // Import the file system module
+const fs = require('fs');  // Import the file system module
 
 const fileName = 'example.txt';
 
-//Write to a file 
-fs.writeFile(fileName, 'Hello, Node.JS!', (err)=>{
-    if(err) throw err;
-    console.log('File created and content written.')
-} )
+// Write to a file
+fs.writeFile(fileName, 'Hello, Node.js!', (err) => {
+    if (err) throw err;
+    console.log('File created and content written.');
+    
+    // Read the file
+    fs.readFile(fileName, 'utf8', (err, data) => {
+        if (err) throw err;
+        console.log(`File Content: ${data}`);
+
+        // Append new data
+        fs.appendFile(fileName, '\nAppending new data!', (err) => {
+            if (err) throw err;
+            console.log('Data appended successfully.');
+
+            // Delete the file
+            fs.unlink(fileName, (err) => {
+                if (err) throw err;
+                console.log('File deleted successfully.');
+            });
+        });
+    });
+});
